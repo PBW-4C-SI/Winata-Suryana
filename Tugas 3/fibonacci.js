99854cfc74cede84db49c539d093963114004de0
Winata17 <suryanawinata9@gmail.com>
2025-03-09 22:56:25 +0700

    Delete Tugas 3 directory

--- Tugas 3/fibonacci.js
+++
@@ -1,9 +0,0 @@
-const fibonacci = (angka) => {
-    const winata = [0, 1];
-    for (let i = 2; i < angka; i++) {
-        winata(winata[i - 1] + winata[i - 2]);
-    }
-    return winata;
-};
-
-console.log(fibonacci(10));
